672759ffcc3cf9a8f7b2a33e2c0056cfabeae8d6
diego <diegocarrp@gmail.com>
2022-07-16 02:11:57 -0400

    added flyway configuration and populate tables

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,25 +1,44 @@
-CREATE TABLE item
+create table sale
 (
-    id           serial  not null,
-    item_name    varchar not null,
-    sku          varchar not null,
-    category_id  numeric not null,
-    price        numeric not null,
-    item_type_id numeric not null,
+    id          serial    not null,
+    datetime    timestamp not null,
+    cashier_id  numeric   not null,
+    customer_id numeric   not null,
+    total       numeric   not null,
+    primary key (id)
+);
+
+create table sale_item
+(
+    sale_id  numeric not null,
+    sku      varchar not null,
+    quantity numeric not null,
+    total    numeric not null,
+    primary key (sale_id, sku)
+);
+
+create table item
+(
+    id               serial  not null,
+    item_name        varchar not null,
+    sku              varchar not null,
+    price            numeric not null,
+    item_category_id numeric not null,
+    item_type_id     numeric not null,
     primary key (id)
 );
 
 create table item_category
 (
-    id            serial  not null,
-    category_name varchar not null,
+    id          serial  not null,
+    description varchar not null,
     primary key (id)
 );
 
 create table item_type
 (
-    id        serial  not null,
-    type_name varchar not null,
+    id          serial  not null,
+    description varchar not null,
     primary key (id)
 );
 
@@ -27,16 +46,9 @@ create table payment_method
 (
     id                  serial  not null,
     description         varchar not null,
-    payment_category_id numeric not null,
     primary key (id)
 );
 
-create table payment_category
-(
-    id            serial  not null,
-    category_name varchar not null,
-    primary key (id)
-);
 
 create table payment
 (
@@ -45,15 +57,3 @@ create table payment
     amount            numeric not null,
     primary key (id)
 );
-
-insert into category (name)
-values ('Food');
-insert into category (name)
-values ('Furniture');
-insert into category (name)
-values ('Delivery');
-
-insert into item_type (name)
-values ('Product');
-insert into item_type (name)
-values ('Service');
---
+++ src/main/resources/db/migration/V2__populate_tables.sql
@@ -0,0 +1,7 @@
+insert into payment_method (description) values ('CASH');
+insert into payment_method (description) values ('CREDIT CARD');
+insert into payment_method (description) values ('DEBIT CARD');
+insert into payment_method (description) values ('DISCOUNT');
+
+insert into item_category (description) values ('PRODUCT');
+insert into item_category (description) values ('SERVICE');

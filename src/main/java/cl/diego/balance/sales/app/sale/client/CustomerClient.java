eb12a4b33e73d3fab92f432d411a221b0eee1c1c
diego <diegocarrp@gmail.com>
2023-03-26 23:51:09 -0300

    fixed feign

--- src/main/java/cl/diego/balance/sales/app/sale/client/CustomerClient.java
+++ src/main/java/cl/diego/balance/sales/app/sale/client/CustomerClient.java
@@ -2,15 +2,16 @@ package cl.diego.balance.sales.app.sale.client;
 
 import cl.diego.balance.sales.app.sale.client.dto.CustomerDto;
 import cl.diego.balance.sales.app.sale.config.feign.CustomerClientConfig;
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "customers" )
+@FeignClient(name = "customers" , configuration = CustomerClientConfig.class, url = "${balance-sales.config.customer.endpoint}")
 public interface CustomerClient {
 
-    @RequestMapping( method = RequestMethod.GET , value = "/by-id/{id}" )
+    @GetMapping
+    @RequestLine( "GET /by-id/{id}" )
     CustomerDto findById( @PathVariable Long id );
 
 }

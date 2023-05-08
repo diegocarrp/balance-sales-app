152f0c39395be86486e134d7de23043f6f1619e5
diego <diegocarrp@gmail.com>
2023-05-08 01:28:51 -0400

    added exception test for controller

--- src/test/java/cl/diego/balance/sales/app/sale/controller/SaleControllerShould.java
+++ src/test/java/cl/diego/balance/sales/app/sale/controller/SaleControllerShould.java
@@ -1,10 +1,12 @@
 package cl.diego.balance.sales.app.sale.controller;
 
+import cl.diego.balance.commons.rest.exception.ApiValidationException;
 import cl.diego.balance.sales.app.sale.dto.SaleDto;
 import cl.diego.balance.sales.app.sale.dto.request.SaleRequest;
 import cl.diego.balance.sales.app.sale.service.SaleService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +14,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.util.NestedServletException;
 
 import java.io.IOException;
+import java.util.List;
 
 import static cl.diego.balance.commons.testing.UtilForTesting.getMappedObjectFromFile;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -57,5 +63,21 @@ public class SaleControllerShould {
                         .content( mapper.writeValueAsString( saleRequest ) ) )
                 .andExpect( status( ).isOk( ) )
                 .andExpect( jsonPath( "$.customerId" ).value( 123 ) );
+    }
+
+    @Test
+    void throwException_givenInvalidRequest( ) {
+        Exception exception =
+                new ApiValidationException( "Error doing test.", List.of( "Error 1 - Error description" ) );
+        when( saleService.registerSale( saleRequest ) )
+                .thenThrow( exception );
+
+        NestedServletException e = assertThrows( NestedServletException.class, ( ) -> {
+            mvc.perform( post( "/sale/register" )
+                    .contentType( MediaType.APPLICATION_JSON )
+                    .content( mapper.writeValueAsString( saleRequest ) ) );
+        } );
+
+        assertEquals( e.getCause( ).getClass( ), ApiValidationException.class );
     }
 }
